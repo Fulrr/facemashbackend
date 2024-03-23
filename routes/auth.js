fa49cd3f9c25cb6ccc8a00cae928f25724ac7b1a
Yossy Papd <122671554+Gifsho@users.noreply.github.com>
2024-03-23 23:52:38 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -35,7 +35,7 @@ router.post('/getUsedetail', authController.getUsedetail);
 router.get(
     '/checkToken', authController.checkToken);
 
-router.get('/findID/:id', authController.finduserId);
+router.get('/findID:id', authController.finduserId);
       
 
 module.exports = router;
