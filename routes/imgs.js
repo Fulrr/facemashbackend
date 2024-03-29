d58d7a66cc1b227445b390445d4c5106c97ff1af
Yossy Papd <122671554+Gifsho@users.noreply.github.com>
2024-03-29 07:16:01 +0700

    Update imgs.js

--- routes/imgs.js
+++ routes/imgs.js
@@ -17,6 +17,6 @@ router.post('/add-image', imgsController.upload);
 router.delete('/delete/:id', imgsController.delete);
 router.get('/fetchAllUserImg/:userId', imgsController.fetchAllUserImg);
 router.get('/fetchAllUserDetail/:facemashId', imgsController.fetchAllUserDetail);
-
+router.put('/changeImg',  imgsController.changeImage);
 
 module.exports = router;
